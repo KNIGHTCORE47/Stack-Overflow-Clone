13bb76da9f81a61a8bb3140302ec4721dd08e93d
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-07-31 12:42:59 +0530

    Fix naming convension

---
+++ src/models/server/storageSetup.ts
@@ -0,0 +1,36 @@
+import { Permission } from "node-appwrite";
+
+import { questionAttatchmentBucket } from '../name'
+import { storage } from './config'
+
+
+export default async function getOrCraeteStorage() {
+    try {
+        await storage.getBucket(questionAttatchmentBucket);
+        console.log("Storage connected");
+
+    } catch (error) {
+        try {
+            await storage.createBucket(
+                questionAttatchmentBucket,
+                questionAttatchmentBucket,
+                [
+                    Permission.read("any"),
+                    Permission.create("users"),
+                    Permission.read("users"),
+                    Permission.update("users"),
+                    Permission.delete("users")
+                ],
+                false,
+                undefined,
+                undefined,
+                ["jpg", "png", "gif", "jpeg", "webp", "heic"]
+            );
+            console.log("Storage created");
+            console.log("Storage connected");
+        } catch (error) {
+            console.log("Error: Unable to create Storage", error);
+        }
+    }
+}
+
